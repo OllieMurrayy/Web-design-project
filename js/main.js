fdd9a9aec2ecb4fc16bc5016d981eacd6e5da515
OllieMurrayy <x22323283@student.ncirl.ie>
2023-12-05 15:28:16 +0000

    Home page work

--- js/main.js
+++ js/main.js
@@ -7,3 +7,20 @@ multiline
 comment
 
 */
+const gallery = document.getElementById('gallery');
+const popup = document.getElementById('popup');
+const selectedImage = documet.getElementById('selectedImage');
+const imageIndexes = [1,2,3,4];
+const selectedIndex = null;
+
+imageIndexes.forEach(i =>{
+    const image = document.createElement('img');
+    image.src= '/images/cars.jpg';
+    image.classList.add('galleryImg');
+    gallery.appendChild(image);
+    addEventListener('click',() =>{
+        //popup stuff
+
+        
+    })
+});
