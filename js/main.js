3d5d1d570d45d0e944772f314921982bfbe19266
OllieMurrayy <x22323283@student.ncirl.ie>
2023-12-09 14:21:17 +0000

    form validation

--- js/main.js
+++ js/main.js
@@ -18,3 +18,66 @@ function openFullImg(pic) {
 function closeFullImg() {
     fullImgBox.style.display = "none";
 }
+
+const form = document.getElementById("form");
+const firstname = document.getElementById("firstname");
+const lastname = document.getElementById("lastname");
+const email = document.getElementById("email");
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+
+    validateInputs();
+});
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector(".error");
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add("error");
+    inputControl.classList.remove("success")
+
+}
+
+const setSuccess = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector(".error");
+    errorDisplay.innerText = "";
+    inputControl.classList.add("success");
+    inputControl.classList.remove("error")
+
+
+};
+
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+
+}
+
+const validateInputs = () => {
+    const firstnameValue = firstname.value.trim();
+    const lastnameValue = lastname.value.trim();
+    const emailValue = email.value.trim();
+
+    if (firstnameValue === "") {
+        setError(firstname, "First name is required")
+    } else {
+        setSuccess(firstname);
+    }
+    
+    if (secondnameValue === "") {
+        setError(lastname, "Last name is required")
+    } else {
+        setSuccess(lastname);
+    }
+
+    if (emailValue === "") {
+        setError(email, "email is required")
+    } else if (!isValidEmail(emailValue)) {
+            setError(email, 'Provide a valid email address');
+    } else {
+        setSuccess(email);
+    }
+};
